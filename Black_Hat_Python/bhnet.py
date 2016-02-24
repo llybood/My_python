8114bfd1e0329dda81bf527657b57be440dfcd54
toufire <taxuewuhenbb@126.com>
2016-02-24 14:22:14 +0800

    change the indent

--- Black_Hat_Python/bhnet.py
+++ Black_Hat_Python/bhnet.py
@@ -51,39 +51,36 @@ def main():
         print str(err)
         usage()
 	for o,a in opts:
-		if o in ("-h","--help"):
-			usage()
-		elif o in ("-l","--listen"):
-			listen = True
-		elif o in ("-e","--execute"):
-			execute = a
-		elif o in ("-c","--commandshell"):
-			command = True
-		elif o in ("-u","--upload"):
-			upload_destination = a
-		elif o in ("-t","--target"):
-			target = a
-		elif o in ("-p","--port"):
-			port = int(a)
-		else:
-			assert False,"Unhandled Option"
+	    if o in ("-h","--help"):
+                usage()
+	    elif o in ("-l","--listen"):
+		listen = True
+	    elif o in ("-e","--execute"):
+		execute = a
+	    elif o in ("-c","--commandshell"):
+		command = True
+	    elif o in ("-u","--upload"):
+		upload_destination = a
+	    elif o in ("-t","--target"):
+		target = a
+	    elif o in ("-p","--port"):
+		port = int(a)
+	    else:
+		assert False,"Unhandled Option"
         
 	#are we going to listen or just send data from stdin?
-	if not listen and len(target) and port > 0:
-    
-		#read in the buffer from the commandline
-		#this will block,so send CTRL-D if not sending input
-		#to stdin
-		buffer = sys.stdin.read()
-    
-		#send data off
-		client_sender(buffer)
-    
-		#we are going to listen and potentially
-		#upload things,execute commands,and drop a shell back
-		#depending on our command line options above
-		if listen:
-			server_loop()
+        if not listen and len(target) and port > 0:
+            #read in the buffer from the commandline
+	    #this will block,so send CTRL-D if not sending input
+	    #to stdin
+	    buffer = sys.stdin.read()
+            #send data off
+	    client_sender(buffer)
+            #we are going to listen and potentially
+	    #upload things,execute commands,and drop a shell back
+	    #depending on our command line options above
+	    if listen:
+		server_loop()
 
 def client_sender(buffer):
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -219,6 +216,7 @@ def client_handler(client_socket):
                 
     
         
-main()       
+main()
+
             
             
