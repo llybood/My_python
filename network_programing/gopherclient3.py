e88774decfe1d37de60b03f2d78824988333c5c9
toufire <taxuewuhenbb@126.com>
2016-04-19 15:19:30 +0800

    change an small error

--- network_programing/gopherclient3.py
+++ network_programing/gopherclient3.py
@@ -4,7 +4,7 @@ __author__ = "coolfire"
 import socket,sys
 host = sys.argv[1]
 port = 70
-filename = sys.argv[3]
+filename = sys.argv[2]
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((host,port))
