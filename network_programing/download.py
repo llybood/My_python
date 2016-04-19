499d504c23baca9d877fdf6cbc47e43f9116f872
toufire <taxuewuhenbb@126.com>
2016-04-19 15:56:25 +0800

    urllib库的使用

---
+++ network_programing/download.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+__author__ = "coolfire"
+
+import urllib,sys
+
+f = urllib.urlopen(sys.argv[1])
+
+while 1:
+	buf = f.read(2048)
+	if not len(buf):
+		break
+	sys.stdout.write(buf)
---
+++ network_programing/urlclient.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+__author__ = "coolfire"
+
+import urllib,sys
+
+host = sys.argv[1]
+file = sys.argv[2]
+
+f = urllib.urlopen('gopher://%s%s' % (host,file))
+for line in f.readlines():
+	sys.stdout.write(line)
