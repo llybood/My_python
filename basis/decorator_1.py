e79bec174bd780117b49a243054f7482295a9cc9
coolfire <taxuewuhenbb@126.com>
2016-04-19 23:52:41 +0800

    同上

--- basis/decorator_1.py
+++ basis/decorator_1.py
@@ -1,2 +1,53 @@
 #-*- coding: utf-8 -*-
 __author__ = 'coolfire'
+
+'''
+def myfunc():
+    print("myfunc() called.")
+
+myfunc()
+myfunc()
+'''
+'''
+def deco(func):
+    print "before myfunc() called."
+    func()
+    print "after myfunc() called."
+    return func
+
+def myfunc():
+    print "myfunc() called."
+
+myfunc = deco(myfunc)
+myfunc()
+myfunc()
+'''
+'''
+import time
+
+def foo():
+    print "in foo()"
+
+def timeit(func):
+    def wrapper():
+        start = time.clock()
+        func()
+        end = time.clock()
+        print 'used:', end-start
+
+    return wrapper
+
+foo = timeit(foo)
+foo()
+'''
+
+def test1():
+    def test2():
+        x = 1
+        y = 2
+        print x + y
+    return test2
+
+test3 = test1
+test3() 
+
--- basis/decorator_2.py
+++ basis/decorator_2.py
@@ -1,2 +1,20 @@
 #-*- coding: utf-8 -*-
 __author__ = 'coolfire'
+
+def getTalk(type='shout'):
+
+    def shout(word='yes'):
+        return word.capitalize()+"!"
+
+    def whisper(word='yes'):
+        return word.lower()+"..."
+
+    if type == 'shout':
+        return shout
+    else:
+        return whisper
+
+
+talk = getTalk()
+print talk()
+
