6867927158a9a1c4fc9aec4297f393ed70200d07
toufire <taxuewuhenbb@126.com>
2016-04-19 13:49:11 +0800

    学习python网络编程基础的第一个小程序

---
+++ network_programing/gopherclient.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+__author__ = "coolfire"
+
+import socket,sys
+host = sys.argv[1]
+port = 70
+filename = sys.argv[2]
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
+
+s.sendall(filename + "\r\n")
+
+while 1:
+	buf = s.recv(2048)
+	if not len(buf):
+		break
+	sys.stdout.write(buf)
+
+
