1274238cd1eab524bfd5bbd3b858efe087e36819
coolfire <taxuewuhenbb@126.com>
2016-05-06 00:16:44 +0800

    Network_Programing_cookbook exercise

---
+++ Network_Programing_cookbook/chapter_1/002.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+__author__ = 'coolfire'
+
+import socket
+from binascii import hexlify
+def convert_ipv4_address():
+    for ip_addr in ['127.0.0.1','192.168.0.1']:
+        packed_ip_addr = socket.inet_aton(ip_addr)
+        unpacked_ip_addr = socket.inet_ntoa(packed_ip_addr)
+        print "ip addrss: %s => Packed: %s,unpacked: %s" % (ip_addr,hexlify(packed_ip_addr),unpacked_ip_addr)
+
+if __name__ == "__main__":
+    convert_ipv4_address()
---
+++ Network_Programing_cookbook/chapter_1/003.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+__author__ = 'coolfire'
+
+import socket
+
+def convert_integer():
+    data = 1234
+    print "Original: %s => Long host byte order: %s,Network byte order: %s"\
+    % (data,socket.ntohl(data),socket.htonl(data))
+    print "Original: %s => Short host byte order: %s, Network byte order: %s"\
+    % (data,socket.ntohs(data),socket.htons(data))
+
+if __name__ == "__main__":
+    convert_integer()
