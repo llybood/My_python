a7e215aa002a3994cd3c25ef2f532cee7e42de64
coolfire <taxuewuhenbb@126.com>
2016-05-13 00:52:37 +0800

    添加一些注释

--- Network_Programing_cookbook/chapter_1/009.py
+++ Network_Programing_cookbook/chapter_1/009.py
@@ -7,16 +7,17 @@ import sys
 import time
 
 NTP_SERVER = '0.uk.pool.ntp.org'
-TIME1970 = 2208988800L
+TIME1970 = 2208988800L  #1970年1月1日NTP时间戳
+
 
 def sntp_client():
     client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    data = '\x1b' + 47 * '\0'
+    data = '\x1b' + 47 * '\0'  #\0相当于\x00,客户端发送给NTP服务器的数据,总共48字节数据,后面47位以数据0填充
     client.sendto(data,(NTP_SERVER,123))
     data,address = client.recvfrom(1024)
     if data:
         print "Response received from: ",address
-    t = struct.unpack('!12I',data)[10]
+    t = struct.unpack('!12I',data)[10] #用struct函数处理二进制数据
     t -= TIME1970
     print '\tTime=%s' % time.ctime()
 
