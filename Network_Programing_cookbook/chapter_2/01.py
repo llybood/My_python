d0abbce3cbd7165a5ee4e694764355c1dfb88264
toufire <taxuewuhenbb@126.com>
2016-05-30 13:40:49 +0800

    modify a error

--- Network_Programing_cookbook/chapter_2/01.py
+++ Network_Programing_cookbook/chapter_2/01.py
@@ -61,7 +61,7 @@ def main():
 	# Launch the server
 	server = ForkingServer((SERVER_HOST,SERVER_PORT),ForkingServerRequestHandler)
 	ip,port = server.server_address
-	server_thread = threading.Thread(target=server.serve_forver())
+	server_thread = threading.Thread(target=server.serve_forever())
 	server_thread.setDaemon(True)
 	server_thread.start()
 	print 'Server Loop running PID: %s' % os.getpid()
