6573a15270fbb3784eb5a6cda0a2dd58335b4118
toufire <taxuewuhenbb@126.com>
2016-06-01 16:57:51 +0800

    测试stdout.flush的用法

---
+++ basis/stdout_flush.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+__author__ = "coolfire"
+import time
+import sys
+
+for i in range(10):
+    print i
+    if i == 5:
+        print "Flushing buffer"
+        sys.stdout.flush()
+    time.sleep(1)
+
+for i in range(10):
+    print i,
+    if i == 5:
+        print "Flushing buffer"
+        sys.stdout.flush()
