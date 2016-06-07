fce66736a1521e1e2f68b1051bb88a2001fe21bd
toufire <taxuewuhenbb@126.com>
2016-06-07 14:37:54 +0800

    添加一些注释

--- Network_Programing_cookbook/chapter_9/01.py
+++ Network_Programing_cookbook/chapter_9/01.py
@@ -30,13 +30,14 @@ def main():
 	iface,port = given_args.iface,given_args.port
 	#start sniffing
 	pc = pcap.pcapObject()
-	pc.open_live(iface,1600,0,100)
-	pc.setfilter('dst port %d' %port,0,0)
+	pc.open_live(iface,1600,0,100)         #参数:设备名(设置为"any"或者为NULL时,则指定所有接口),数据包最大长度,是否设置为混杂模式(False则设置成混杂),超时时间ms)
+	pc.setfilter('dst port %d' %port,0,0)  #参数:编译规则字符串,优化选项,网络掩码
+	#netmask specifies the IPv4 netmask of the network on which packets are being captured; it is used only when checking for IPv4 broadcast addresses in the filter program. If the netmask of the network on which packets are being captured isn't known to the program, or if packets are being captured on the Linux "any" pseudo-interface that can capture on more than one network, a value of 0 can be supplied; tests for IPv4 broadcast addreses won't be done correctly, but all other tests in the filter program will be OK
 
 	print 'Press CTRL+C to end capture'
 	try:
 		while True:
-			pc.dispatch(1,print_packet)
+			pc.dispatch(1,print_packet)    #参数:返回前捕获多少数据包,回调函数
 	except KeyboardInterrupt:
 			print "Packet statistics: %d packets received, %d packets dropped,%d packets dropped by the interface" % pc.stats()
 
