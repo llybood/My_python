98949412b8d21d464c3b22bcd0e2130b83426d10
toufire <taxuewuhenbb@126.com>
2016-07-15 17:17:03 +0800

    关于construct模块的一些学习测试

--- construct_test/fields_01.py
+++ construct_test/fields_01.py
@@ -1,5 +1,38 @@
 #-*- coding: utf-8 -*-
 __author__ = "coolfire"
-from construct import UBInt16,ULInt16
+from construct import Adapter,Bytes,Construct,Struct
+from construct import UBInt16,ULInt16,Byte
+
 UBInt16("foo").parse("\x01\x02")
 ULInt16("foo").parse("\x01\x02")
+
+class IpAddressAdapter(Adapter):
+	def _encode(self,obj,context):
+		return "".join(chr(int(b)) for b in obj.split())
+	def _decode(self,obj,context):
+		return ".".join(str(ord(b)) for b in obj)
+
+#IpAddressAdapter(Bytes("foo",4)).parse("\x01\x02\x03\x04")
+
+
+
+class PrintContext(Construct):
+	def _parse(self, stream, context):
+		print context
+		print "hello"
+
+foo = Struct("foo",
+			 Byte("a"),
+			 Byte("b"),
+			 PrintContext("c"),
+			 Struct("bar",
+					Byte("a"),
+					Byte("b"),
+					PrintContext("c"),
+					),
+			 PrintContext("d"),
+			 )
+
+foo.parse("\x01\x02\x03\x04")
+
+
