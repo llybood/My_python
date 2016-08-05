afccf452b10f6fec88b4134fd51f8ec87847fe40
toufire <taxuewuhenbb@126.com>
2016-08-05 18:40:11 +0800

    路由追踪脚本

--- Shenma_autotest/top_cashback.py
+++ Shenma_autotest/top_cashback.py
@@ -7,14 +7,19 @@ password = "admin@321.com"
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get(login_url)
-now_handle = driver.current_window_handle
-print now_handle
 driver.find_element_by_id('username').send_keys(username)
 driver.find_element_by_id('password').send_keys(password)
 driver.find_element_by_css_selector(css_selector="a.ui.fluid.large.teal.button").click()
+
 #点击会员管理
-driver.find_element_by_css_selector(css_selector="#nav > div:nth-child(3) > div.panel-header.panel-header-noborder.accordion-header > div.panel-title.panel-with-icon").click()
-driver.find_element_by_css_selector(css_selector="#nav > div:nth-child(3) > div.panel-body.panel-body-noborder.accordion-body > div > ul > li:nth-child(3) > div > a > span.nav").click()
-#driver.find_element_
+try:
+	element = WebDriverWait(driver,60).until(EC.title_is(u"系统管理"))
+except:
+	print u"系统异常"
+	driver.quit()
+finally:
+	driver.find_element_by_css_selector(css_selector="#nav > div:nth-child(3) > div.panel-header.panel-header-noborder.accordion-header > div.panel-title.panel-with-icon").click()
+	driver.find_element_by_css_selector(css_selector="#nav > div:nth-child(3) > div.panel-body.panel-body-noborder.accordion-body > div > ul > li:nth-child(3) > div > a > span.nav").click()
