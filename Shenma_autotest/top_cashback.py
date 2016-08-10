948564e26678b406c9073662f6ca27689930892e
toufire <taxuewuhenbb@126.com>
2016-08-10 18:37:15 +0800

    汉众合伙人返利自动测试系统编写

--- Shenma_autotest/top_cashback.py
+++ Shenma_autotest/top_cashback.py
@@ -7,6 +7,7 @@ password = "admin@321.com"
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get(login_url)
@@ -22,4 +23,9 @@ except:
 	driver.quit()
 finally:
 	driver.find_element_by_css_selector(css_selector="#nav > div:nth-child(3) > div.panel-header.panel-header-noborder.accordion-header > div.panel-title.panel-with-icon").click()
+	element1 = WebDriverWait(driver,60).until(EC.visibility_of_element_located((By.CSS_SELECTOR,"#nav > div:nth-child(3) > div.panel-body.panel-body-noborder.accordion-body > div > ul > li:nth-child(3) > div > a > span.nav")))
 	driver.find_element_by_css_selector(css_selector="#nav > div:nth-child(3) > div.panel-body.panel-body-noborder.accordion-body > div > ul > li:nth-child(3) > div > a > span.nav").click()
+	driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="tabPanel-35"]/iframe'))
+	elem = WebDriverWait(driver,60).until(EC.visibility_of_element_located((By.CSS_SELECTOR,"body > div.article > table > tbody > tr:nth-child(2) > td:nth-child(9)")))
+	rowCount = driver.find_element_by_css_selector(css_selector="body > div.article > table > tbody")
+	#charge = driver.find_element_by_css_selector(css_selector="body > div.article > table > tbody > tr:nth-child(3cd j) > td:nth-child(9)").text
