2443a8a853446eda6df4b44d8ef08312b1e0c4eb
coolfire <taxuewuhenbb@126.com>
2016-08-17 23:18:33 +0800

    修改糗事百科的程序(正则匹配有所改变)

--- My_Tools/get_qiushibaike.py
+++ My_Tools/get_qiushibaike.py
@@ -26,12 +26,12 @@ class QSBK:
         try:
             url = 'http://www.qiushibaike.com/hot/page/' + str(pageIndex)
             #构建请求的request
-            request = urllib2.Request(url,headers = self.headers)
+            request = urllib2.Request(url,headers = self.headers,)
             #利用urlopen获取页面代码
-            response = urllib2.urlopen(request)
+            response = urllib2.urlopen(request,timeout=10)
             #将页面转化为UTF-8编码
             pageCode = response.read()
-            return pageCode.decode("utf-8")
+            return pageCode
         except urllib2.URLError,e:
             if hasattr(e,'reason'):
                 print "连接糗事百科失败,错误原因",e.reason
@@ -40,29 +40,32 @@ class QSBK:
     #传入某一页代码,返回本页不带图片的段子列表
     def getPageItems(self,pageIndex):
         pageCode = self.getPage(pageIndex)
+        #print pageCode
         if not pageCode:
             print '页面加载失败....'
             return None
-        pattern = re.compile('<div.*?author.*?alt="(.*?)".*?content">(.*?)<!--(.*?)-->.*?</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
+        pattern = re.compile('<div.*?author.*?alt="(.*?)".*?content">(.*?)</div>.*?number">(.*?)</i>',re.S)
 
         items = re.findall(pattern,pageCode)
         #用来存储每页的段子
         pageStories = []
         #遍历正则表达式匹配的信息
         for item in items:
+            #print item[0],item[1],item[2]
+            #print item
             #是否含有图片
-            haveImg = re.search("img",item[3])
+            #haveImg = re.search("img",item[3])
             #如果不含有图片,把它加入list中
-            if not haveImg:
-                replaceBR = re.compile('<br/>')
-                text = re.sub(replaceBR,"\n",item[1])
+            #if not haveImg:
+            #    replaceBR = re.compile('<br/>')
+            #    text = re.sub(replaceBR,"\n",item[1])
                 #item[0]是一个段子的发布者,item[1]是内容,item[2]是发布时间,item[4]是点赞人数
-                pageStories.append([item[0].strip(),text.strip(),item[2].strip(),item[4].strip()])
+            pageStories.append([item[0].strip(),item[1].strip(),item[2].strip()])
         return pageStories
 
     #加载并提取页面的内容,加入到列表中
     def loadPage(self):
-        #如果当前未看的页数少于2,则加载新的一页:
+        #如果当前未看的页面数少于2,则加载新的一页:
         if self.enable == True:
             if len(self.stories) < 2:
                 #获取新的一页
@@ -72,6 +75,8 @@ class QSBK:
                     self.stories.append(pageStories)
                     #获取完以后页码索引加一,表示下次读取下一页
                     self.pageIndex += 1
+                    print self.pageIndex
+
 
 
     #调用该方法,每次敲回车打印输出一个段子
@@ -86,7 +91,7 @@ class QSBK:
             if input == "Q":
                 self.enable = False
                 return
-            print "第%d页\t发布人:%s\t发布时间:%s\t赞:%s\n%s" % (page,story[0],story[2],story[3],story[1])
+            print "第%d页\t发布人:%s\t赞:%s\n%s" % (page,story[0],story[2],story[1])
 
     #开始方法
     def start(self):
@@ -109,6 +114,7 @@ class QSBK:
                 self.getOneStory(pageStories,nowPage)
 
 spider = QSBK()
+#spider.getPage(1)
+#spider.getPageItems(1)
 spider.start()
-
 #spider.loadPage()
